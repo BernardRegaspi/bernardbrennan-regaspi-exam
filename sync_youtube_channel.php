fceef3a1770cf10d9fc33b06c006aeb93ba44c7c
BernardRegaspi <bernard-regaspi.pixel8@gmail.com>
2023-09-16 08:40:04 +0800

    exam

--- sync_youtube_channel.php
+++ sync_youtube_channel.php
@@ -2,18 +2,17 @@
 
 include "youtube_channel_json.php";
 
-// Accessing data
-echo $channel_data["channelName"]; // "Net Ninja"
-echo $channel_data["channelDescription"]; // Description text
-echo $channel_data["channelProfilePicture"]; // Profile picture URL
+echo $channel_data["channelName"];
+echo $channel_data["channelDescription"]; 
+echo $channel_data["channelProfilePicture"];
 
-// Accessing video data
+
 $video = $channel_data["videos"][0];
-echo $video["videoTitle"]; // "Build a Microservice with Go #4 - Data Model"
-echo $video["videoDescription"]; // Video description
-echo $video["videoUrl"]; // Video URL
-echo $video["videoId"]; // Video ID
-echo $video["videoThumbnail"]; // Video Thumbnail
+echo $video["videoTitle"];
+echo $video["videoDescription"]; 
+echo $video["videoUrl"];
+echo $video["videoId"]; 
+echo $video["videoThumbnail"]; 
 
 // print_r($channel_data);
 
@@ -22,10 +21,8 @@ $username = "root";
 $password = "";
 $database = "youtube_db";
 
-// Create a connection
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -34,24 +31,19 @@ $channelName = $channel_data["channelName"];
 $channelProfilePicture = $channel_data["channelProfilePicture"];
 $channelDescription = $channel_data["channelDescription"];
 
-// Prepare the SQL statement
 $sql = "INSERT INTO youtube_channels (channel_name, channel_profile, channel_description)
         VALUES (?, ?, ?)";
 
-// Create a prepared statement
 $stmt = $conn->prepare($sql);
 
-// Bind parameters
 $stmt->bind_param("sss", $channelName, $channelProfilePicture, $channelDescription);
 
-// Execute the statement
 if ($stmt->execute()) {
     echo "Channel data inserted successfully.";
 } else {
     echo "Error: " . $stmt->error;
 }
 
-// Close the statement
 $stmt->close();
 
 $videos = $channel_data["videos"];
@@ -61,26 +53,21 @@ foreach ($videos as $video) {
     $videoUrl = $video['videoUrl'];
     $videoDescription = $video['videoDescription'];
     $videoId = $video['videoId'];
-    $videoThumbnail = $video['videoThumbnail']; // Add Video Thumbnail
+    $videoThumbnail = $video['videoThumbnail'];
 
-    // Prepare the SQL statement
     $sql = "INSERT INTO youtube_channel_videos (id_channel, video_id, video_title, video_url, video_description, video_thumbnail)
             VALUES ((SELECT id_channel FROM youtube_channels WHERE channel_name = ?), ?, ?, ?, ?, ?)";
 
-    // Create a prepared statement
     $stmt = $conn->prepare($sql);
 
-    // Bind parameters
     $stmt->bind_param("ssssss", $channelName, $videoId, $videoTitle, $videoUrl, $videoDescription, $videoThumbnail);
 
-    // Execute the statement
     if ($stmt->execute()) {
         echo "Video data inserted successfully.";
     } else {
         echo "Error: " . $stmt->error;
     }
 
-    // Close the statement
     $stmt->close();
 }
 
--- youtube_channel_json.php
+++ youtube_channel_json.php
@@ -1,48 +1,28 @@
 <?php
 $api_key = "AIzaSyBgd0Hk3RJ1JnphC7zeraE3fLwwmWeYXZw";
 $channel_id = "UCW5YeuERMmlnqo4oq8vwUpg";
-$max_results_per_page = 50; // Number of results to fetch per page
-$total_results = 100; // Total number of results to retrieve
+$max_results_per_page = 50;
+$total_results = 100;
 
-// Initialize an array to store channel and video data
 $channel_data = array();
-
-// Calculate the number of pages needed to retrieve the desired results
 $total_pages = ceil($total_results / $max_results_per_page);
-
-// Initialize an array to store all video data
 $all_videos = array();
 
-// Loop through each page and make API requests
 for ($page = 1; $page <= $total_pages; $page++) {
-    // Calculate the starting index for the current page
     $start_index = ($page - 1) * $max_results_per_page + 1;
-
-    // Create the API URL for the current page
     $video_url = "https://www.googleapis.com/youtube/v3/search?channelId=$channel_id&order=date&part=snippet&type=video&maxResults=$max_results_per_page&key=$api_key&pageToken=" . ($page > 1 ? $next_page_token : '');
-
-    // Make an HTTP request to fetch video data
     $video_response = file_get_contents($video_url);
 
-    // Check if the video request was successful
     if ($video_response) {
-        // Decode the JSON response for video data
         $video_data = json_decode($video_response, true);
-
-        // Store the next page token for the next request
         $next_page_token = isset($video_data['nextPageToken']) ? $video_data['nextPageToken'] : '';
 
-        // Loop through the videos and add them to the all_videos array
         foreach ($video_data['items'] as $item) {
             $videoTitle = $item['snippet']['title'];
             $videoDescription = $item['snippet']['description'];
             $videoId = $item['id']['videoId'];
             $videoThumbnail = $item['snippet']['thumbnails']['medium']['url'];
-
-            // Generate the YouTube video URL link
             $videoUrl = "https://www.youtube.com/watch?v=$videoId&ab_channel=" . $channel_id;
-
-            // Add video details to the all_videos array
             $all_videos[] = array(
                 'videoTitle' => $videoTitle,
                 'videoDescription' => $videoDescription,
@@ -55,20 +35,17 @@ for ($page = 1; $page <= $total_pages; $page++) {
         $channel_data['channelError'] = "Failed to fetch video data from the YouTube API.";
     }
 
-    // If we have retrieved enough results, break out of the loop
     if (count($all_videos) >= $total_results) {
         break;
     }
 }
 
-// Fetch channel information
 $channel_info_url = "https://www.googleapis.com/youtube/v3/channels?part=snippet&id=$channel_id&key=$api_key";
 $channel_info_response = file_get_contents($channel_info_url);
 
 if ($channel_info_response) {
     $channel_info = json_decode($channel_info_response, true);
 
-    // Extract channel details
     $channel_data['channelName'] = $channel_info['items'][0]['snippet']['title'];
     $channel_data['channelDescription'] = $channel_info['items'][0]['snippet']['description'];
     $channel_data['channelProfilePicture'] = $channel_info['items'][0]['snippet']['thumbnails']['medium']['url'];
@@ -76,24 +53,14 @@ if ($channel_info_response) {
     $channel_data['channelError'] = "Failed to fetch channel information.";
 }
 
-// Combine channel data and all video data
 $channel_data['videos'] = $all_videos;
 
-// Set the response header to indicate JSON content
 header('youtube_channel_json.php');
 header('Content-Type: application/javascript');
 
 $callback = isset($_GET['callback']) ? $_GET['callback'] : 'defaultCallback';
-
-// Your JSON data
-
-// Wrap the JSON data in the callback function
 $response = $callback . '(' . json_encode($channel_data) . ');';
-
-
 
 echo $response;
 
-// Output the JSON data
-// echo json_encode($channel_data);
 ?>
